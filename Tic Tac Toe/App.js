d3a9379c4745698d889fb51d620d0bf8b7b6c004
preetigoddess <147085314+preetigoddess@users.noreply.github.com>
2024-02-07 20:52:51 +0530

    Add files via upload

---
+++ Tic Tac Toe/App.js
@@ -0,0 +1,105 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document. querySelector("#reset-btn");
+let newBtn = document.querySelector("#new-btn");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg")
+
+let turnO = true;
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        console.log("box was Clicked");
+        if(turnO){
+            box.innerText = "O";
+            turnO = false;
+        }
+        else{
+            box.innerText = "X";
+            turnO = true;
+        }
+       box.disabled = true;
+       checkWinner()
+    });
+});
+
+
+const disabledBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations,winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+    disabledBoxes();
+
+}
+
+
+const checkWinner = () => {
+    for(pattern of winPatterns){
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if(pos1val != "" && pos2val != "" && pos3val !=""){
+            if(pos1val === pos2val && pos2val === pos3val){
+                console.log("winner", pos1val);
+                showWinner(pos1val);
+            }
+        }
+    }
+}
+ const resetGame = () => {
+    turnO = true;
+   enableBoxes();
+ 
+
+msgcontainer.classList.add("hide");
+
+    newButtons.forEach((button) => {
+        disableButtonColor(button);
+    });
+
+    
+}
+ newBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+const newButtons = document.querySelectorAll(".box");
+
+const myButton = (button) => {
+    button.style.backgroundColor = "#45a049";
+};
+
+const disableButtonColor = (button) => {
+    button.style.backgroundColor = ""; 
+};
+
+newButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        myButton(button);
+    });
+});
+
+
+
+
